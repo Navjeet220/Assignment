af8b83d0f6c38776f22652a4a664ea6c2bbb9da3
navjeet <ghotranavjeet592@gmail.com>
2018-06-13 15:18:37 +0530

    file added

---
+++ assignment2.py
@@ -0,0 +1,63 @@
+
+                 #ASSIGNMENT2
+
+#QUESTION:1	print anything you want on screen
+#SOLUTION:  
+           #first method
+print("hi this is navjeet")
+			
+		
+					 
+            #second method
+print("hi \n this is navjeet")
+				   
+			#third method
+print("hi\t this\t is\t navjeet")			
+
+#QUESTION:2 Join two strings using '+'.
+#SOLUTION:  
+x="lovey"
+y="ghotra"
+z=x+y
+print(z)
+print(type(z))
+		   
+		   
+#QUESTION:3 take the input of 3 variables x,y and z.print their values on screen.
+#SOLUTION:  
+            #first method
+x=input("enter a number: ")
+y=input("enter an another number: ")
+z=x+y
+print(z)
+print(type(z))
+		   
+		   #second method
+x=int(input("enter a number: "))
+y=int(input("enter an another number: "))
+z=x+y
+print(z)
+print(type(z))
+		   
+		   
+
+#QUESTION:4 Print "Let's get started" on screen.
+#SOLUTION: 
+           
+		   #first method 
+print('"Let\'s get started"')
+		   		  
+		    
+#QUESTION:5 Print the following values using placeholders.
+s="Acadview"
+course="Python"   
+fees=5000 
+#SOLUTION:  
+print("%s %s %d" %(s,course,fees)) 					
+					
+					
+					
+#QUESTION:6  Let's do some interesting exercise:                      
+name="Tony Stark"
+salary=1000000
+print("%s %d"%(name,salary))
